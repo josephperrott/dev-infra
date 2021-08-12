7aea42552052fcf75d72147357436c82cae25697
Joey Perrott <josephperrott@gmail.com>
2021-08-12 12:13:14 -0700

    refactor(github-actions): extract fetching auth token for angular-github-app into utility function

---
+++ github-actions/utils.ts
@@ -0,0 +1,17 @@
+import {getInput} from '@actions/core';
+const {getToken} = require('github-app-installation-token');
+
+
+export async function getAuthTokenForAngularRobotApp(): Promise<string>;
+export async function getAuthTokenForAngularRobotApp(inputKey = 'angular-robot-key'): Promise<string> {
+  /** The private key for the angular robot app. */
+  const privateKey = getInput('angular-robot-key');
+  /** Github App id of the Angular Robot app. */
+  const appId = 43341;
+  /** Installation id of the Angular Robot app. */
+  const installationId = 2813208;
+  // The Angular Lock Bot Github application
+  const {token} = await getToken({installationId, appId, privateKey})
+
+  return token;
+}
